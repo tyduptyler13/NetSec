34545a599ba2a7fbaf721c0b0b8aed34666b8515
Tyler Scott <tyduptyler13@gmail.com>
2016-08-01 16:03:42 -0600

    Fixed animation and button. Removed clients.

--- js/agency.js
+++ js/agency.js
@@ -7,10 +7,10 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
+        var anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 500, 'swing');
         event.preventDefault();
     });
 });
