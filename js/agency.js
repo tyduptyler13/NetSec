a77199db3a49c6edd9f3e52c4bd5fef9dc047676
Tyler Scott <tyduptyler13@gmail.com>
2016-08-08 20:38:13 +0000

    Moved more things to cdn and fixed some broken features.
    * Fixed scroll menu highlighting
    * Moved javascript includes to head
    * Moved all bootstrap to cdn includes
    * Deleted unused template resources

--- js/agency.js
+++ js/agency.js
@@ -6,37 +6,53 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top
-        }, 500, 'swing');
-        event.preventDefault();
+  $('a.page-scroll').bind('click', function(event) {
+    var anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+    }, 500, 'swing');
+    //$('.nav.navbar-nav li.active').removeClass('active');
+    //anchor.parent().addClass('active');
+    event.preventDefault();
+  });
+
+  $(document).scroll(function(){
+    var height = $(document).scrollTop();
+    var target = $('.navbar-fixed-top');
+    if (height < 300 && target.hasClass('navbar-shrink')){
+      target.removeClass('navbar-shrink');
+    } else if (height > 300 && !target.hasClass('navbar-shrink')){
+      target.addClass('navbar-shrink');
+    }
+  });
+
+  $(document).scroll(function(){
+    var pos = $(document).scrollTop() + $(window).height() / 2;
+    $('section').each(function(i, ele){
+      var e = $(ele);
+      if (pos >= e.offset().top){
+        var id = e.attr('id');
+        $('.nav.navbar-nav li.active').removeClass('active');
+        $('.nav.navbar-nav a[href="#' + id + '"]').parent().addClass('active');
+      }
     });
+  })
+
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function() {
+      $('.navbar-toggle:visible').click();
+  });
+
+  $('div.modal').on('show.bs.modal', function() {
+  	var modal = this;
+  	var hash = modal.id;
+  	window.location.hash = hash;
+  	window.onhashchange = function() {
+  		if (!location.hash){
+  			$(modal).modal('hide');
+  		}
+  	}
+  });
 });
 
-$(document).scroll(function(){
-  var height = $(window).scrollTop();
-  var target = $('.navbar-fixed-top');
-  if (height < 300 && target.hasClass('navbar-shrink')){
-    target.removeClass('navbar-shrink');
-  } else if (height > 300 && !target.hasClass('navbar-shrink')){
-    target.addClass('navbar-shrink');
-  }
-});
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
-});
